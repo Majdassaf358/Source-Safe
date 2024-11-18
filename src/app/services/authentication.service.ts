8211b342971538e1146a20971b190fd52299f5d1
Majd.assaf358 <majd.assaf@mozaik.ae>
2024-11-18 11:33:37 +0300

    adding authentications service

---
+++ src/app/services/authentication.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { authenticationReq } from '../models/authenticationReq';
+import { Observable } from 'rxjs';
+import { environment } from '../models/environments';
+import { ApiResponse } from '../models/ApiResponse';
+import { authenticationRes } from '../models/authenticationRes';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthenticationService {
+
+  constructor(private http: HttpClient) { }
+
+  public login(request:authenticationReq): Observable<ApiResponse<authenticationRes>>{
+    let url = `${environment.apiUrl}/register`;
+    return this.http.post<ApiResponse<authenticationRes>>(url,request);
+  }
+
+}
