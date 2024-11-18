8914f772d71997ee546ec7ba678bca4175702aac
Majd.assaf358 <majd.assaf@mozaik.ae>
2024-11-18 18:56:20 +0300

    copying login to register

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,15 +1,20 @@
 import { Routes,RouterModule } from '@angular/router';
 import { LoginComponent } from './components/authentication/login/login.component';
+import { RegisterComponent } from './components/authentication/register/register.component';
 
 export const routes: Routes = [
     {
         path:'',
-        redirectTo:'login',
+        redirectTo:'register',
         pathMatch:'full',
     },
     {
         path: 'login',
         component:LoginComponent,
+    },
+    {
+        path: 'register',
+        component:RegisterComponent,
     },
 ];
 
---
+++ src/app/components/authentication/register/register.component.ts
@@ -0,0 +1,38 @@
+import { Component } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { authenticationReq } from '../../../models/authenticationReq';
+import { AuthenticationService } from '../../../services/authentication.service';
+import {lastValueFrom} from 'rxjs';
+import { ApiResponse } from '../../../models/ApiResponse';
+import { authenticationRes } from '../../../models/authenticationRes';
+
+
+@Component({
+  selector: 'app-register',
+  standalone: true,
+  imports: [FormsModule,
+    ReactiveFormsModule,],
+  templateUrl: './register.component.html',
+  styleUrl: './register.component.css'
+})
+export class RegisterComponent {
+
+  registerReq:authenticationReq = new authenticationReq;
+
+  constructor(private authenticationService:AuthenticationService) {}
+
+  sendFields(request:authenticationReq){
+    this.registerFunction(request);
+  }
+
+  async registerFunction(request:authenticationReq){
+    try{
+      let res:ApiResponse<authenticationRes> = await lastValueFrom(this.authenticationService.register(request));
+      this.registerReq = res.data;
+      console.log('true')
+    }
+    catch(error){
+      console.log(error);
+    }
+  }
+}
