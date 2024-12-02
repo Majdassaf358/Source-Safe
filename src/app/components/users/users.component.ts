edcebe430235afe40caf92c57072ba8ae2093af1
Majd.assaf358 <majd.assaf@mozaik.ae>
2024-12-02 12:06:54 +0300

    users styles + start

--- src/app/components/users/users.component.ts
+++ src/app/components/users/users.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { PopUpComponent } from '../pop-up/pop-up.component';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { UsersService } from '../../services/users.service';
 
 @Component({
   selector: 'app-users',
   standalone: true,
-  imports: [],
+  imports: [PopUpComponent, FormsModule, CommonModule],
   templateUrl: './users.component.html',
-  styleUrl: './users.component.css'
+  styleUrl: './users.component.css',
 })
-export class UsersComponent {
+export class UsersComponent implements OnInit {
+  showPopUp: boolean = false;
+  display: string = '';
+  constructor(private usersService: UsersService) {}
 
+  ngOnInit(): void {}
 }
