2cf09f7e4782e59167a60b9fa79f1eb358939d3b
Majd.assaf358 <majd.assaf@mozaik.ae>
2025-01-05 17:52:35 +0300

    upload files need CORS

--- src/app/components/files/files.component.ts
+++ src/app/components/files/files.component.ts
@@ -1,6 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, FormGroup, FormsModule } from '@angular/forms';
 import { APIarray } from '../../models/APIarray';
 import { viewfile } from '../../models/viewfile';
 import { FilesService } from '../../services/files.service';
@@ -8,11 +8,13 @@ import { ActivatedRoute } from '@angular/router';
 import { lastValueFrom } from 'rxjs';
 import { error } from 'console';
 import { checkIn } from '../../models/check-in';
+import { fileUpload } from '../../models/file-upload';
+import { HttpClient, HttpClientModule, HttpHeaders } from '@angular/common/http';
 
 @Component({
   selector: 'app-files',
   standalone: true,
-  imports: [FormsModule, CommonModule],
+  imports: [FormsModule, CommonModule,HttpClientModule],
   templateUrl: './files.component.html',
   styleUrl: './files.component.css'
 })
@@ -22,15 +24,24 @@ export class FilesComponent implements OnInit{
     mode:string='default';
     tem:string='';
     files: viewfile[] = [];
-    selectedFiles: number[]= [];
+    checkInFiles: number[]= [];
     checkInRes:checkIn[] = [];
+    uploadForm !: FormGroup;
+    currentFile !: File;
+
+    selectedFiles:any[]=[];
+    uploadedFiles:fileUpload[]=[];
 
     constructor(private fileService:FilesService,
-    private route: ActivatedRoute
-
+    private route: ActivatedRoute,
+    private http: HttpClient,
+    private formBuilder : FormBuilder
     ){}
 
   ngOnInit(): void {
+    this.uploadForm = this.formBuilder.group({
+      'file_path[]':[]
+    });
     this.route.paramMap.subscribe((paramsMap) => {
       this.groupName = paramsMap.get('groupName') || '';
       this.viewFiles(this.groupName)
@@ -47,14 +58,14 @@ export class FilesComponent implements OnInit{
   temporary(){}
 
   addNumberToCheckIn(id?:number){
-    this.selectedFiles.push(id || 0);
+    this.checkInFiles.push(id || 0);
     this.checkIn();
   }
 
   async checkIn(){
     try{
       let res: APIarray<checkIn> = await lastValueFrom(
-        this.fileService.checkIn(this.groupName,this.selectedFiles)
+        this.fileService.checkIn(this.groupName,this.checkInFiles)
       );
       this.checkInRes=res.data;
       console.log(res);
@@ -73,5 +84,74 @@ export class FilesComponent implements OnInit{
     } catch (error) {
       console.log(error);
     }
+  }
+
+  seFi:File = {} as File;
+
+  onFileSelected(event:any){
+    // this.selectedFiles = Array.from(event.target.files);
+    this.currentFile = event.target.files[0];
+    this.selectedFiles = event.target.files;
+    this.uploadForm.patchValue({'file_path[]':this.selectedFiles[0]});
+    this.seFi = event.target.files[0];
+  }
+  uploadFiles(){
+    const reqOptions = {
+      headers : new HttpHeaders({
+        'Content-Type': 'application/json',
+        'Accept': 'application/json',
+        'Access-Control-Allow-Origin':'*',
+        'Access-Control-Allow-Methods': '*',
+        'Access-Control-Allow-Headers': '*',
+        Authorization: 'Bearer 5|UCiF4PiheqiSBLwqnDV4YSu7PQqaO7sJXE9yH14ra8e63381'
+      })
+    }
+    const formData = new FormData();
+
+    if(this.currentFile){
+      formData.append("file",this.currentFile);
+    }
+  
+
+    // formData.append('file_path[]',this.selectedFiles[0],this.selectedFiles[0].name);
+    // console.log('here');
+
+    this.http.post(`http://localhost/api/${this.groupName}/upload-file`,formData)
+    .subscribe({
+      next:(res)=>{
+        console.log('success',res);
+      }, error:(error)=>{
+       console.log(error);
+      }
+    })
+
+
+
+    // if (this.selectedFiles.length === 0) return; 
+    
+    // const formData = new FormData();
+
+    // console.log(this.selectedFiles[0]);
+    // console.log();
+
+    // formData.append('file_path[]', 'this.selectedFiles[0]');
+    // formData.append('file_path', 'this.selectedFiles[0]');
+
+    // formData.append('test', 'fi1');
+
+    // console.log(formData.get('test'));
+    // console.log(formData.get('file_path[]'));
+
+    // // this.selectedFiles.forEach(file => {
+    // //   formData.append('file_path[]', file); // Use 'file_path[]' to match backend expectation
+    // // });
+    // this.http.post<any>(`http://127.0.0.1:8000/api/${this.groupName}/upload-file`,formData)
+    // .subscribe({
+    //   next:(res)=>{
+    //     console.log('success',res);
+    //   }, error:(error)=>{
+    //    console.log(error);
+    //   }
+    // })
   }
 }
